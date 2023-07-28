3c8f20654a63a6e818dc312a005a7174ec2b3d36
Tiago <tiago.silva991@outlook.com>
2023-07-28 17:20:30 -0300

    Jogo descobrir numero

---
+++ script.js
@@ -0,0 +1,41 @@
+var numberToFind = 0;
+var attemps = 0;
+
+function refresh() {
+  // gerar um numero aleatorio
+
+  attemps = 0;
+
+  numberToFind = parseInt(Math.random() * 100);
+  console.log(numberToFind)
+}
+
+function verifyNumber() {
+
+  var bet = document.getElementById('bet').value;
+
+  if(bet > 100 || bet < 1)
+  {
+    alert('Aposta inválida')
+    return;
+  }
+
+  if(bet > numberToFind)
+  {
+    attemps++;
+    alert('🚨 O número para ser encontrado é MENOR');
+  }
+  else if(bet < numberToFind) 
+  {
+    attemps++;
+    alert('🚨 O número para ser encontrado é MAIOR');
+  }
+  else
+  {
+    alert('✔️ Parábens você acertou!! Com '+attemps+' erros');
+    refresh();
+  }
+
+}
+
+refresh ()
